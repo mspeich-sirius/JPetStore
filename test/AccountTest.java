922891c5bf031667ce59bd2ce0a1aa048c37fc63
mspeich <mark.speich@siriuscom.com>
2017-04-24 12:17:10 -0500

    Added junit tests

---
+++ test/AccountTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+public class AccountTest {
+	@Test
+	public void test1Test() {
+	}
+	@Test
+	public void test2Test() {
+	}
+	@Test
+	public void test3Test() {
+	}
+	@Test
+	public void test4Test() {
+	}
+	@Test
+	public void test5Test() {
+	}
+	@Test
+	public void test6Test() {
+	}
+	@Test
+	public void test7Test() {
+	}
+	@Test
+	public void test8Test() {
+	}
+}
